6db938a606532ad5efa0917c28323f7c62fc695b
surysharma <surysharma@gmail.com>
2019-06-29 19:38:46 +0100

    Ask Pattern example

--- src/main/scala/com/bigscale/askpattern/AskPatternDriver.scala
+++ src/main/scala/com/bigscale/askpattern/AskPatternDriver.scala
@@ -2,6 +2,9 @@ package com.bigscale.askpattern
 
 import akka.actor.{Actor, ActorSystem, Props}
 
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 object AskPatternDriver extends App {
@@ -12,7 +15,7 @@ object AskPatternDriver extends App {
   object DataAccess {
     def getData() = {
       Thread.sleep(2000)
-      println("Returning content...")
+      println("Returning content from DAO...")
       "Some content from DAO"
     }
   }
@@ -21,7 +24,6 @@ object AskPatternDriver extends App {
   class DataAccessActor extends Actor {
     override def receive: Receive = {
       case GetContent =>
-        println("Inside GetContent actor...")
         sender() ! DataAccess.getData()
       case _ => println("UNKNOWN message....")
     }
@@ -31,17 +33,10 @@ object AskPatternDriver extends App {
 
   val dataAccessActor = system.actorOf(Props(new DataAccessActor()), "DataAccessActor")
 
-  import akka.pattern.ask
-  import akka.util.Timeout
-  import scala.concurrent.duration._
   implicit val timeout = Timeout(5 seconds)
   implicit val context = system.dispatcher
   val respFuture: Future[Any] = (dataAccessActor ? GetContent)
-
-  println("response returned " + respFuture)
-
-  val result = Await.result(respFuture, timeout.duration).asInstanceOf[String]
-
+  val result = Await.result(respFuture, timeout.duration).asInstanceOf[String] //Blocking call!!!
   println("Result from blocking call: " + result)
 
   system.stop(dataAccessActor)
